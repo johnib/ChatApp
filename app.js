6d01d196e9445a8986be7aab8da2002ec3f7fd91
Asaf Savich <asafsavich@gmail.com>
2016-05-11 00:28:18 +0300

    First try emit/on

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function (data) {
+    io.on('new message', function (msg) {
+        console.log(msg.toString());
+    });
+});
+
+io.on('connect', function () {
+    io.emit('new message', 'Hello server');
+});
 
 
-app.listen(3000, function () {
+server.listen(3000, function () {
 
 });
