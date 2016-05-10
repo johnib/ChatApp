ab5f31d049e8580821a3a6342defa96fbf0f9c3f
Asaf Savich <asafsavich@gmail.com>
2016-05-11 02:12:23 +0300

    Resolved conflict

--- app.js
+++ app.js
@@ -2,14 +2,37 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connect', function () {
-  console.log('new connection');
-  io.emit('new message', 'Hello server');
+// function sendToAllButSender(socket, msg) {
+//     for (var current in io.sockets.sockets) {
+//         if (current.id != socket.id ) {
+//             current.emit('public message', msg);
+//         }
+//     }
+// }
+
+
+io.on('connection', function (socket) {
+    console.log("connection");
+
+    socket.emit("user login", socket.username);
+
+
+    socket.on('new message', function (msg) {
+        console.log(msg.data.toString());
+        //socket.emit('own message', msg);
+        msg.sender = socket.id;
+        io.emit('new message', msg);
+
+    });
+
+    io.emit('new user'); // TODO - Add user name
 });
 
-io.on('new message', function (data) {
-  console.log(data);
-});
+//
+// io.on('connect', function () {
+//     console.log("connect");
+// });
+
 
 server.listen(3000, function () {
 
